765eb32fee6a1357f13e4f9f7b65e73a003d8912
CKA <jwang252@ncsu.edu>
2021-09-08 20:52:49 -0700

    Longer Format

---
+++ Long Format Data set/Roll up household member.R
@@ -0,0 +1,19 @@
+library(readxl)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+
+tgcp_demog <- read_excel("C:/Users/CKA/Downloads/STATCOM_data.xlsx", col_types = "text")
+
+tgcp_demog <- rename(tgcp_demog, Id = `...1`)
+
+
+Longer_Format <- pivot_longer(tgcp_demog,col = 20:117,names_to = "HHMemberInfo",values_to = "HHMemberInfoAnswer",values_drop_na = TRUE)
+
+
+
+
+library(reshape2)
+#New <-melt(tgcp_demog,na.rm = TRUE, id.vars = c(names(tgcp_demog)[-20:-117]),measure.vars = c(names(tgcp_demog)[20:117]),names_pattern = "(.)(.)")
+#names(longer)[31] <- "HHMInfoAnswer"
+#names(longer)[32] <- "HHMInfo"
