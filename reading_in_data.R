ec8e3304a9bb05b9c8b6f0ee652b7ded77651482
AlvinSheng <asheng1067@gmail.com>
2021-09-10 14:15:02 -0400

    exploring the tgcp data a bit

--- reading_in_data.R
+++ reading_in_data.R
@@ -15,3 +15,14 @@ tgcp_demog <- rename(tgcp_demog, Id = `...1`)
 View(tail(tgcp_demog[, c(1:40, 119:128)], n = 100))
 
 
+
+# Exploration of tgcp_demog data
+
+# Proportion of missing values in each column
+apply(tgcp_demog, 2, function(x) mean(is.na(x)))
+# Question: at which time(s) do each column generally start having not-NA values? 
+
+# About 22% of clients referred via Wake County Public School System
+mean(tgcp_demog$Agency == "WCPSS", na.rm = T) 
+
+
